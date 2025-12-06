afe7ffe87da9317de827151db55749450821899d
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-07 01:57:35 +0700

    refactor: change nav icon and modifying query builder

--- app/Filament/Resources/Timesheets/TimesheetResource.php
+++ app/Filament/Resources/Timesheets/TimesheetResource.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Timesheets;
 
+use App\Enum\Cuti\StatusPengajuan;
+use App\Enum\Permission;
+use App\Enum\Role;
+use App\Enum\Timesheet\StatusPersetujuan;
 use App\Filament\Resources\Timesheets\Pages\CreateTimesheet;
 use App\Filament\Resources\Timesheets\Pages\EditTimesheet;
 use App\Filament\Resources\Timesheets\Pages\ListTimesheets;
@@ -15,12 +19,14 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class TimesheetResource extends Resource
 {
     protected static ?string $model = Timesheet::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCalendarDateRange;
 
     protected static ?string $recordTitleAttribute = 'karyawan.nama_lengkap';
 
@@ -49,5 +55,30 @@ class TimesheetResource extends Resource
             'view' => ViewTimesheet::route('/{record}'),
             'edit' => EditTimesheet::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $query = parent::getEloquentQuery();
+        $user = Auth::user();
+
+        if ($user->hasAnyPermission(Permission::APPROVE_MANAGE_TIMESHEET, Permission::REJECT_MANAGE_TIMESHEET)) {
+            return $query->where('status', StatusPersetujuan::Diteruskan);
+        }
+
+        if ($user->hasPermissionTo(Permission::DIRECT_MANAGE_TIMESHEET)) {
+            return $query->whereIn('status', [
+                StatusPersetujuan::Dibuat,
+                StatusPersetujuan::Dilihat
+            ]);
+        }
+
+        // Karyawan hanya bisa lihat cuti milik sendiri
+        if ($user->hasAnyPermission(Permission::EDIT_MANAGE_TIMESHEET,Permission::DELETE_MANAGE_TIMESHEET,Permission::CREATE_MANAGE_TIMESHEET)) {
+            $karyawanId = \App\Models\User::getKaryawanId($user);
+            return $query->where('karyawan_id', $karyawanId);
+        }
+
+        return $query;
     }
 }
