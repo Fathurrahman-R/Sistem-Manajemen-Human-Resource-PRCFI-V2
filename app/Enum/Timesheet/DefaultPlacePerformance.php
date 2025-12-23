fbb6365cb1d7b50e23e829a3151f1cc324609643
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-23 09:50:48 +0700

    add: implementing has label trait

--- app/Enum/Timesheet/DefaultPlacePerformance.php
+++ app/Enum/Timesheet/DefaultPlacePerformance.php
@@ -2,9 +2,17 @@
 
 namespace App\Enum\Timesheet;
 
-enum DefaultPlacePerformance: string
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Contracts\Support\Htmlable;
+
+enum DefaultPlacePerformance: string implements HasLabel
 {
     case Pontianak = 'Pontianak';
     case KapuasHulu = 'Kapuas Hulu';
     case Sintang = 'Sintang';
+
+    public function getLabel(): string|Htmlable|null
+    {
+        return $this->value;
+    }
 }
