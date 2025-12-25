1ddc7f0f62fe2672196000a0b06d3910c2d5c986
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-25 17:45:51 +0700

    change: changing form layout

--- app/Filament/Schemas/IsiTimesheetForm.php
+++ app/Filament/Schemas/IsiTimesheetForm.php
@@ -4,9 +4,13 @@ namespace App\Filament\Schemas;
 
 use App\Enum\Timesheet\Location;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
+use Filament\Schemas\Components\FusedGroup;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 use IsiTimesheet;
 
 class IsiTimesheetForm
@@ -15,15 +19,35 @@ class IsiTimesheetForm
     {
         return $schema
             ->components([
-                ToggleButtons::make('location')->options(Location::class)->inline(),
-                DatePicker::make('tanggal')
-                    ->displayFormat('d F Y')
-                    ->label(__('Date'))
-                    ->default(now())
-                    ->native(false),
-                ToggleButtons::make('jam_bekerja')->options([8=>'Satu hari',4=>'Setengah hari']),
-                TextInput::make('place'),
-                TextInput::make('work_done')
+                ToggleButtons::make('location')
+                    ->required()
+                    ->hiddenLabel()
+                    ->options(Location::class)
+                    ->inline(),
+                Group::make([
+                    FusedGroup::make([
+                        DatePicker::make('tanggal')
+                            ->required()
+                            ->prefixIcon(Heroicon::Calendar)
+                            ->displayFormat('d M Y')
+                            ->label(__('Date'))
+                            ->default(now())
+                            ->native(false),
+                        TextInput::make('place')
+                            ->required()
+                            ->prefixIcon(Heroicon::MapPin)
+                            ->columnSpan(2),
+                    ])
+                        ->label('Date and place')
+                        ->columns(3)
+                        ->columnSpan(2),
+                    ToggleButtons::make('jam_bekerja')
+                        ->required()
+                        ->inline()
+                        ->options([8=>'Satu hari',4=>'Setengah hari']),
+                ])->columns(3),
+                Textarea::make('work_done')
+                    ->required()
             ])->columns(1);
     }
 }
